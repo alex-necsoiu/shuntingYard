61b842782a14392e66ea06be1c01d734652ea6d4
Ioan Alexandru Necsoiu <alex@MacBook-Pro-de-Ioan-3.local>
2023-03-21 14:59:12 +0100

    feat: Add more testcases

--- pkg/main_test.go
+++ pkg/main_test.go
@@ -11,10 +11,25 @@ func TestEvaluateExpression(t *testing.T) {
 		expected float64
 	}{
 		{"5 + 3", 8},
+		{"1 + 2", 3},
+		{"1 - 2", -1},
+		{"2 * 3", 6},
+		{"6 / 3", 2},
+		{"1 + 2 * 3", 7},
 		{"1 + 2 * 5", 11},
+		{"(1 + 2) * 3", 9},
 		{"(1+2) * 5", 15},
 		{"(1+2) * (3+2)", 15},
 		{"1 / (3 - 4)", -1},
+		{"6 / (2 + 1)", 2},
+		{"8 / 2 * (2 + 2)", 16},
+		{"2 * (3 + 4 * 5) - 6", 40},
+		{"(5 - 3) * (4 + 6 / 2)", 14},
+		{"9 + 24 / (7 - 3)", 15},
+		{"(3.5 + 2.5) * 4", 24},
+		{"(4 - 2) * (4.5 + 1.5)", 12},
+		{"(3.1 + 4.9) * 2", 16},
+		{"7 / 3.5", 2},
 	}
 
 	for _, tc := range testCases {
@@ -26,6 +41,6 @@ func TestEvaluateExpression(t *testing.T) {
 		if result != tc.expected {
 			t.Errorf("Expected %f, got %f", tc.expected, result)
 		}
-		fmt.Printf("Input: %s = Expected: %f => Result: %f\n", tc.input, tc.expected, result)
+		fmt.Printf("Input: %s || Expected: %f => Result: %f\n", tc.input, tc.expected, result)
 	}
 }
